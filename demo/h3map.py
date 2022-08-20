ae94332ea06fbe8fcd80e00b44003b96147412ad
rupert <rupert@he360.com>
2022-08-20 18:40:02 +0000

    code cleanup

--- demo/h3map.py
+++ demo/h3map.py
@@ -13,20 +13,19 @@ async def worker_task(work_queue, out_stream):
     """
     fetches h3 indices & uses api to look up nearest coastal point, write result to <out_stream>
     """
-    def nearest_coast_distance_url(h):
-        center = h3.h3_to_geo(h)
-        return f"http://0.0.0.0:8001/ne/nearest_coast.json?longitude={center[1]}&latitude={center[0]}"
+    def nearest_coast_distance_url(longitude, latitude):
+        return f"http://0.0.0.0:8001/ne/nearest_coast.json?longitude={longitude}&latitude={latitude}"
 
     async with aiohttp.ClientSession() as session:
         while not work_queue.empty():
             h = await work_queue.get()
-            async with session.get(nearest_coast_distance_url(h)) as response:
+            lat, lon = h3.h3_to_geo(h)
+            async with session.get(nearest_coast_distance_url(lon, lat)) as response:
                 try:
-                    center = h3.h3_to_geo(h)
                     r = await response.json()
                     if not r['ok'] or not r['rows'][0][0]:
                         raise Exception(f"error fetching index {h}, response: {r}")
-                    out_stream.write(f"""{h}, {center[1]}, {center[0]}, {r['rows'][0][0]}\n""")
+                    out_stream.write(f"""{h}, {lon}, {lat}, {r['rows'][0][0]}\n""")
                 except Exception as e:
                     print(f"error: {e}")
                     await work_queue.put(h)
@@ -38,8 +37,8 @@ async def produce_locations(work_queue, res: int, min_lon, max_lon, min_lat, max
     """
 
     def in_bounds(h):
-        center = h3.h3_to_geo(h)
-        return (center[1] >= min_lon) and (center[1] <= max_lon) and (center[0] >= min_lat) and (center[0] <= max_lat)
+        lat, lon = h3.h3_to_geo(h)
+        return (lon >= min_lon) and (lon <= max_lon) and (lat >= min_lat) and (lat <= max_lat)
 
     indices: set = set()
     for h0 in h3.get_res0_indexes():
@@ -64,5 +63,5 @@ async def main(res: int,
 
 
 if __name__ == "__main__":
-    # asyncio.run(main(res=3, max_lat=42, min_lat=35, max_lon=30, min_lon=24))
-    asyncio.run(main(res=0))
+    asyncio.run(main(res=3, max_lat=42, min_lat=35, max_lon=30, min_lon=24))
+    # asyncio.run(main(res=0))
